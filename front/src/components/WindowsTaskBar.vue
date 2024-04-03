888b0e7c97031d633fb562de495100dde81812e1
Suylo <spoownz@yahoo.com>
2024-04-03 10:40:04 +0200

    fix(front): img

--- front/src/components/WindowsTaskBar.vue
+++ front/src/components/WindowsTaskBar.vue
@@ -1,10 +1,32 @@
 <script setup lang="ts">
-import BgTaskbar from "./_icons/BgTaskbar.vue";
 </script>
 
 <template>
-  <BgTaskbar/>
+  <div class="taskbar">
+    <img src="/img/start_btn.png" alt="">
+  </div>
 </template>
 
 <style scoped lang="scss">
+.taskbar{
+  position: fixed;
+  z-index: -1;
+  width: 100vw;
+  height: 51px;
+  left: 0;
+  bottom: 0;
+  background: #245DDB;
+  box-shadow: inset 0 4px 4px rgba(109, 178, 255, 0.7), inset 0px -5px 7px rgba(0, 0, 0, 0.22), inset 0px 10px 8px #3888E9;
+
+  img{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+
+    &:hover{
+      cursor: pointer;
+      opacity: .9;
+    }
+  }
+}
 </style>
