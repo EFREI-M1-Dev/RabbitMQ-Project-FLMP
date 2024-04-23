e11b4dc116c9813050e1f532373bbd285c53549c
Florent Paris <florentparis28@gmail.com>
2024-04-23 14:02:27 +0200

    fix(windows-menu): menu wip

---
+++ front/src/components/WindowMenu.vue
@@ -0,0 +1,147 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="menu">
+    <div class="top">
+      <div><img alt="" src="" /></div>
+      <span>Username</span>
+    </div>
+    <div class="body">
+      <ul>
+        <li>
+          <img />
+          <div>
+            <span>Internet</span>
+            <span>Internet Explorer</span>
+          </div>
+        </li>
+        <li>
+          <img />
+          <div>
+            <span>E-mail</span>
+            <span>Outlook Express</span>
+          </div>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <img />
+          <span>My Documents</span>
+        </li>
+        <li>
+          <img />
+          <span>My Recent Documents</span>
+        </li>
+        <li>
+          <img />
+          <span>My Pictures</span>
+        </li>
+        <li>
+          <img />
+          <span>My Music</span>
+        </li>
+      </ul>
+    </div>
+    <div class="bottom">
+      <div>Log Off</div>
+      <div>Turn Off Computer</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.menu {
+  width: 400px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  bottom: 38px;
+  border-radius: 8px 8px 0 0;
+  border: 2px solid rgb(59, 114, 186);
+  overflow: hidden;
+
+  .top {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    color: #fff;
+    background: rgb(67, 143, 234);
+    background: linear-gradient(
+      0deg,
+      rgba(67, 143, 234, 1) 0%,
+      rgba(11, 95, 206, 1) 100%
+    );
+    padding: 12px 8px;
+    border-bottom: 1px solid #4c73a1;
+    box-shadow: inset #3897ff 0 2px 2px 0;
+
+    img {
+      width: 30px;
+      aspect-ratio: 1/1;
+      border: 3px solid #f0f;
+    }
+  }
+
+  .body {
+    display: flex;
+
+    > ul {
+      width: 100%;
+    }
+
+    > ul:first-of-type {
+      border-right: 1px solid #bbcad5;
+
+      > li {
+        padding: 10px;
+        white-space: nowrap;
+        display: flex;
+        align-items: center;
+        gap: 4px;
+
+        img {
+          width: 20px;
+          aspect-ratio: 1/1;
+        }
+
+        > div {
+          display: flex;
+          flex-direction: column;
+
+          > span:first-of-type {
+            font-size: 14px;
+          }
+
+          > span:last-of-type {
+            font-size: 13px;
+          }
+        }
+      }
+    }
+
+    > ul:last-of-type {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      gap: 4px;
+
+      li {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+
+        img {
+          width: 20px;
+          aspect-ratio: 1/1;
+        }
+
+        span {
+          white-space: nowrap;
+          font-size: 14px;
+        }
+      }
+    }
+  }
+}
+</style>
--- front/src/components/WindowsTaskBar.vue
+++ front/src/components/WindowsTaskBar.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import WindowsTab from "@/components/WindowsTab.vue";
-import {Tab} from "@/_interfaces/Tab.ts";
+import WindowsTab from '@/components/WindowsTab.vue';
+import WindowMenu from '@/components/WindowMenu.vue';
+import { Tab } from '@/_interfaces/Tab.ts';
 
 defineProps<{
-  tabs: Tab[]
-}>()
+  tabs: Tab[];
+}>();
 
 function formatTime() {
   const now = new Date();
@@ -16,28 +17,31 @@ function formatTime() {
   return `${hours}:${minutes} ${ampm}`;
 }
 
-function logout(){
+function logout() {
   localStorage.removeItem('user');
   location.reload();
 }
 </script>
+<!--  -->
 
 <template>
   <div class="taskbar">
-    <img src="/img/start_btn.png" alt="" title="Déconnexion" @click="logout">
+    <WindowMenu />
+
+    <img src="/img/start_btn.png" alt="" title="Déconnexion" @click="logout" />
 
     <div class="taskbar__tabs">
       <WindowsTab v-for="tab in tabs" :key="tab.title" :tab="tab" />
     </div>
 
     <div class="taskbar__tray">
-      {{formatTime()}}
+      {{ formatTime() }}
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.taskbar{
+.taskbar {
   font-family: 'Source Sans 3', sans-serif;
   position: fixed;
   display: flex;
@@ -48,34 +52,41 @@ function logout(){
   height: 4.7vh;
   left: 0;
   bottom: 0;
-  background: #245DDB;
-  box-shadow: inset 0 4px 4px rgba(109, 178, 255, 0.7), inset 0px -5px 7px rgba(0, 0, 0, 0.22), inset 0px 10px 8px #3888E9;
+  background: #245ddb;
+  box-shadow: inset 0 4px 4px rgba(109, 178, 255, 0.7),
+    inset 0px -5px 7px rgba(0, 0, 0, 0.22), inset 0px 10px 8px #3888e9;
 
-  img{
+  img {
     position: absolute;
     left: 0;
     bottom: 0;
     height: 4.7vh;
 
-    &:hover{
+    &:hover {
       cursor: pointer;
-      opacity: .9;
+      opacity: 0.9;
     }
   }
 
-  &__tabs{
+  &__tabs {
     margin-left: 170px;
     display: flex;
-    gap: .125rem;
+    gap: 0.125rem;
     height: 100%;
     align-items: center;
   }
 
-  &__tray{
+  &__tray {
     color: white;
-    background: linear-gradient(180deg, rgb(32 176 251) 0%, rgb(14 137 234) 50%, rgb(21 147 233) 100%);
+    background: linear-gradient(
+      180deg,
+      rgb(32 176 251) 0%,
+      rgb(14 137 234) 50%,
+      rgb(21 147 233) 100%
+    );
     box-sizing: border-box;
-    box-shadow: inset #5a89a0 -2px 0 3px 0, inset #3a546e 0 -2px 3px 0, inset #e2e7ff 0 2px 3px 0;
+    box-shadow: inset #5a89a0 -2px 0 3px 0, inset #3a546e 0 -2px 3px 0,
+      inset #e2e7ff 0 2px 3px 0;
     height: 100%;
     display: flex;
     align-items: center;
